92d738de354e1dafd3eff86400ab83ad6bc55155
S-Shimotori <S-Shimotori@S-Shimotoris-MacBook-Pro.local>
2015-05-15 12:39:21 +0900

    add cells to TableViewController
    set identifer and TableViewCell

--- TableViewUpdates/TableViewController.swift
+++ TableViewUpdates/TableViewController.swift
@@ -9,25 +9,37 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    private let identifier = "Cell"
+    private let uiLabelText = ["にんじん","たまねぎ","じゃがいも"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nib = UINib(nibName: "TableViewCell", bundle: nil)
+        tableView.registerNib(nib, forCellReuseIdentifier: identifier)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 3
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-        
+        let cell =  tableView.dequeueReusableCellWithIdentifier(identifier) as! TableViewCell
+        cell.uiLabel.text = uiLabelText[indexPath.row]
+        cell.uiSwitch.tag = indexPath.row
+        cell.uiSwitch.addTarget(self, action: "changeUISwitchValue:", forControlEvents: .ValueChanged)
         return cell
+    }
+    
+    func changeUISwitchValue(sender: UISwitch) {
+        println("\(sender.tag) \(!sender.on)->\(sender.on)")
     }
 
 }
